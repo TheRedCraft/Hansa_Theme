629612ac10d7e4a89387fed3f207f6644874dc85
TheRedCraft <bananeananas29@gmail.com>
2022-09-27 18:36:00 +0200

    yes

--- functions.php
+++ functions.php
@@ -1,10 +1,13 @@
 <?php
 
-function load_style()
+function enqueue_styles()
 {
     wp_enqueue_style('main', get_template_directory_uri() . '/style.css');
 }
 
+add_action("wp_enqueue_scripts", "enqueue_styles");
+
+/*
 function make_nav_menu()
 {
     register_nav_menus( array(
@@ -12,3 +15,11 @@ function make_nav_menu()
     ));
     }
 add_action('wp_enqueue_scripts', 'load_style', 'make_nav_menu');
+*/
+function setup_theme() {
+    register_nav_menus(array(
+        'header-menu' => 'Header Menü',
+        'footer-menu' => 'Footer Menü'
+    ));
+}
+add_action('after_setup_theme', "setup_theme");
