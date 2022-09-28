d3054a846e0dbdfb24f85db4413be6883ef406a2
TheRedCraft <bananeananas29@gmail.com>
2022-09-28 18:21:59 +0200

    yes

--- index.php
+++ index.php
@@ -8,8 +8,14 @@ if(have_posts())
     {
         the_post();
 
-        echo "<div class'POST-ID'>";
+        echo "<div class'twitch-box'>";
+        echo "<div class='content'>";
         the_title('<h2>', '</h2>');
+        echo "</div>";
+        echo "<div class='twitch-border-effect'>";
+        echo "<div class='top-corner'></div>";
+        echo "<div class='bottom-corner'></div>";
+        echo "</div>";
         echo "</div>";
     }
 } else
