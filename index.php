a27c13437b89e7412f06f3e28b1d850104b9d1e3
kreuterkeule <moritz.siefke@protonmail.com>
2022-10-01 18:29:18 +0200

    new postloop for post in category 'Startseite'

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <h1>News</h1>
 
-<?php 
+<?php
 
 if(have_posts())
 {
@@ -29,8 +29,31 @@ if(have_posts())
         {
             echo '';
         }
-    } 
+    }
     echo "</div>";
+} else {
+  echo "No Posts where Found";
+}
+wp_reset_postdata();
+if (have_posts()) {
+  while (have_posts() && $i <= 7) {
+    the_post();
+
+    if(in_category('Startseite'))
+    {
+      echo "<div class='twitch-box'>";
+      echo "<div class='content'>";
+      the_post_thumbnail();
+      the_title('<h2>', '</h2>');
+      echo "</div>";
+      echo "<div class='twitch-border-effect'>";
+      echo "<div class='top-corner'></div>";
+      echo "<div class='bottom-corner'></div>";
+      echo "</div>";
+      echo "</div>";
+      $i += 1;
+    }
+  }
 } else
 {
     echo 'No Posts where Found';
