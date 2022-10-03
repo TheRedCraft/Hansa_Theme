1e9d065916e82c22f02d3d9fa164c3c51f40360a
kreuterkeule <moritz.siefke@protonmail.com>
2022-10-03 14:35:59 +0200

    single.php ready for styling

--- single.php
+++ single.php
@@ -6,9 +6,13 @@ if(have_posts())
 {
     while(have_posts())
     {
-        the_post();
-        the_title();
-        the_content();
+      the_post();
+      echo "<div class='single-post'>";
+        the_title('<h2 class"single-post-heading>', '</h2>');
+        echo "<div class='single-post-content'>";
+          the_content();
+        echo "</div>";
+      echo "</div>";
     }
 }
 ?>
