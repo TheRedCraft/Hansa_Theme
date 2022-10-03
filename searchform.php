7725d8a26e09668d61fbed11ef097d5aabe6b548
kreuterkeule <moritz.siefke@protonmail.com>
2022-10-03 11:44:02 +0200

    try to repair searchform

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form action="/wordpress" method="get">
+<form action="/wordpress/archive.php" method="get">
 	<input type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
 	<input type="submit" alt="Search"/>
 </form>
