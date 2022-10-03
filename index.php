50b9bcd95c78755c589d763b2b4aacf0b8e75dbb
kreuterkeule <moritz.siefke@protonmail.com>
2022-10-03 14:25:57 +0200

    copy to index.php

--- index.php
+++ index.php
@@ -1,79 +1,72 @@
 <?php get_header(); ?>
 
-<h1 class="section-heading">News</h1>
-
 <?php
 
 $args = array(
-  'post_type' => 'post',
-  'post_status' => 'publish',
   'posts_per_page' => 8,
+  'post_status' => 'publish',
+  'post_type' => 'post',
   'category_name' => 'News'
 );
 
 $loop = new WP_Query($args);
+
+if($loop->have_posts())
+{
+  echo "<div class='twitch-boxen'>";
+  while($loop->have_posts())
+  {
+    $loop->the_post();
+    echo "<a href'".the_permalink(get_the_ID())."'>";
+    echo "<div class='twitch-box'>";
+    echo "<div class='content'>";
+    the_post_thumbnail();
+    the_title('<h2 class="twitch-head">', '</h2>');
+    echo "</div>";
+    echo "<div class='twitch-border-effect'>";
+    echo "<div class='top-corner'></div>";
+    echo "<div class='bottom-corner'></div>";
+    echo "</div>";
+    echo "</div>";
+    echo "</a>";
+  }
+  echo "</div>";
+}
+
 ?>
-<?php if( $loop->have_posts()): ?>
-  <div class='twitch-boxen'>
-  <?php while($loop->have_posts()):?>
-    <?php $loop->the_post(); $id = get_the_ID(); ?>
-    <a href="/wordpress/">
-    <div class='twitch-box'>
-    <div class='content'>
-    <?php the_post_thumbnail(); the_title('<h2 class="twitch-head">', '</h2>');?>
-    </div>
-    <div class='twitch-border-effect'>
-    <div class='top-corner'></div>
-    <div class='bottom-corner'></div>
-    </div>
-    </div>
-    </a>
-  <?php endwhile; ?>
-  <p>test</p>
-  </div>
-  <?php endif; ?>
+
+<?php wp_reset_postdata(); ?>
 
 <?php
 
-wp_reset_postdata();
-
-echo "<h1 class='section-heading'>Startseite</h1>";
-
 $args2 = array(
-  'post_type' => 'post',
-  'post_status' => 'publish',
   'posts_per_page' => 8,
+  'post_status' => 'publish',
+  'post_type' => 'post',
   'category_name' => 'Startseite'
 );
 
 $loop2 = new WP_Query($args2);
-?>
 
-<?php if( $loop2->have_posts()): ?>
-  <div class='twitch-boxen'>
-  <?php while($loop2->have_posts()):?>
-    <?php $loop2->the_post();
-    $id = get_the_ID(); ?>
-    <a href="/wordpress/">
-    <div class='twitch-box'>
-    <div class='content'>
-    <?php the_post_thumbnail();
-    the_title('<h2 class="twitch-head">', '</h2>');?>
-    </div>
-    <div class='twitch-border-effect'>
-    <div class='top-corner'></div>
-    <div class='bottom-corner'></div>
-    </div>
-    </div>
-    </a>
-  <?php endwhile; ?>
-  </div>
-  <?php endif; ?>
-
-<?php
-
-echo 'Index PHP';
+if($loop2->have_posts())
+{
+  echo "<div class='twitch-boxen'>";
+  while($loop2->have_posts())
+  {
+    $loop2->the_post();
+    echo "<div class='twitch-box'>";
+    echo "<div class='content'>";
+    the_post_thumbnail();
+    the_title('<h2 class="twitch-head">', '</h2>');
+    echo "</div>";
+    echo "<div class='twitch-border-effect'>";
+    echo "<div class='top-corner'></div>";
+    echo "<div class='bottom-corner'></div>";
+    echo "</div>";
+    echo "</div>";
+  }
+  echo "</div>";
+}
 
 ?>
-
 <?php get_footer(); ?>
