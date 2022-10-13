da3fbf9a621de76e0801c3e7dd3f273ab166e4bf
kreuterkeule <moritz.siefke@protonmail.com>
2022-10-13 17:12:46 +0200

    start of twitch-box-wichtig and better 404.php

--- 404.php
+++ 404.php
@@ -1,7 +1,46 @@
 <?php 
 get_header();
 
-echo "ERROR 404: Leider nichts gefunden, diese Seite gibt es nicht.";
+echo "<div style=' margin-left: 5vw; height: 20vh; width: 100%;'></div>";
+
+echo "<h2 style=' margin-left: 5vw; '>ERROR 404: Leider nichts gefunden, diese Seite gibt es nicht.</h2>";
+
+echo "<br><br><h2 style=' margin-left: 5vw; '>Eine zufaellige Seite findest du hier:</h2>";
+
+$args = array(
+    'post_type' => 'post',
+    'post_status' => 'publish',
+    'posts_per_page' => 1,
+    'category__not_in' => array(get_cat_ID('sliderImg')),
+    'orderby' => 'rand'
+  );
+
+  $loop = new WP_Query($args);
+
+  if( $loop->have_posts())
+  {
+    echo "<div class='twitch-boxen'>";
+    while($loop->have_posts())
+    {
+      $loop->the_post();
+      echo "<a href='".get_the_permalink()."'>";
+      echo "<div class='twitch-box'>";
+      echo "<div class='content'>";
+      the_post_thumbnail();
+      the_title('<h2 class="twitch-head">', '</h2>');
+      echo "</div>";
+      echo "<div class='twitch-border-effect'>";
+      echo "<div class='top-corner'></div>";
+      echo "<div class='bottom-corner'></div>";
+      echo "</div>";
+      echo "</div>";
+      echo "</a>";
+    }
+    echo "</div>";
+  } else {
+    echo "No Searchresults found";
+  }
+
 
 get_footer();
 ?>
--- home.php
+++ home.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
 
-<h2 class="section-heading">News</h2>
 <?php
 
 /* Test */
@@ -16,24 +15,35 @@ $wichtigLoop = new WP_Query($argsWichtig);
 
 if($wichtigLoop->have_posts())
 {
+  echo "<div class='twitch-boxen'>";
   while($wichtigLoop -> have_posts())
   {
     $wichtigLoop->the_post();
-    echo "<div class'wichtig-box'>";
-    echo "<div class'wichtig-heading'>";
-    the_title();
+    echo "<a href='".get_the_permalink()."'>";
+    echo "<div class='twitch-box-wichtig'>";
+    echo "<div class='content'>";
+    echo "<div class='wichtig-col1'>";
+    the_post_thumbnail();
+    the_title('<h2 class="twitch-head">', '</h2>');
     echo "</div>";
-    echo "<div class'wichtig-content'>";
+    echo "<div class='wichtig-col2'>";
     the_content();
     echo "</div>";
-    echo "<div class'wichtig-thumbnail'>";
-    the_post_thumbnail();
+    echo "</div>";
+    echo "<div class='twitch-border-effect'>";
+    echo "<div class='top-corner'></div>";
+    echo "<div class='bottom-corner'></div>";
     echo "</div>";
     echo "</div>";
+    echo "</a>";
   }
+  echo "</div>";
 }
 
 wp_reset_postdata();
+
+echo '<h2 class="section-heading">News</h2>';
+
 
 $args = array(
   'posts_per_page' => 8,
