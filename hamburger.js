db1e1ab37ac306abe7bfa99d2dd49c38e7f8435d
kreuterkeule <moritz.siefke@protonmail.com>
2022-10-24 10:07:21 +0200

    try to fix mobile menu

--- hamburger.js
+++ hamburger.js
@@ -1,25 +1,31 @@
 if ("ontouchstart" in document.documentElement)
 {
+  console.log('mobile');
   hamburger = document.querySelector('.hamburger')
   navMenu = document.querySelector('.menu-header-menu-container')
 
   hamburger.addEventListener('click', () => {
+    console.log('click hamburger')
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
   });
-  hamburger.addEventListener('touchstart', () => {
+  hamburger.addEventListener('touchend', () => {
+    console.log('hamburger touch-start')
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
-  });
+  }, false);
   document.querySelectorAll('.menu-item').forEach(n => {
+    console.log('document.querySelectorAll did work')
     n.addEventListener('click', () =>  {
+      console.log('click menu-item');
       hamburger.classList.remove('active');
       navMenu.classList.remove('active');
     })
-    n.addEventListener('touchstart', () =>  {
+    n.addEventListener('touchend', () =>  {
+        console.log('touchstart menu-item')
         hamburger.classList.remove('active');
         navMenu.classList.remove('active');
-      })
+      },false)
   })
 }
 
