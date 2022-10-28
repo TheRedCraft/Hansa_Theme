cac4231ae2983a00f63dd6ac54e4b7dc1dea7db6
TheRedCraft <111310570+TheRedCraft@users.noreply.github.com>
2022-10-28 21:53:22 +0200

    so gut wie fertig!!!!!!

--- hamburger.js
+++ hamburger.js
@@ -16,7 +16,7 @@ if ("ontouchstart" in document.documentElement)
     }
   });
 
-  document.querySelectorAll('.menu-item').forEach(n => {
+  /*document.querySelectorAll('.menu-item').forEach(n => {
     if(n.classList.contains('menu-item-has-childes') == false) {
     n.addEventListener('click', () =>  {
       hamburger.classList.remove('active');
@@ -24,27 +24,40 @@ if ("ontouchstart" in document.documentElement)
       navMenu.classList.add('notActive');
     })
   }
-  })
+  })*/
 
-  
   let submenu_list = document.querySelectorAll('.sub-menu');
   for(q = 0; q < submenu_list.length; q++) {
-    submenu_list[q].classList.add('notshow')
+    submenu_list[q].style.display = 'none';
   }
 
   let has_childes = document.querySelectorAll('.menu-item-has-children');
   for(i = 0; i < has_childes.length; i++) {
-    a_as_childes = has_childes.querySelector('a')
-    has_childes.querySelector('.sub-menu').innerHTML += a_as_childes;
+    a_as_childes = has_childes[i].querySelector('a')
+    a_as_childes2 = has_childes[i].querySelector('a').href
+    a_as_childes3 = has_childes[i].querySelector('a').textContent;
     a_as_childes.href = '#';
+    a_as_childes2 = "<a href='" + a_as_childes2 + "' aria-current='page'>" + a_as_childes3 + "</a>"
+
+    has_childes[i].querySelector('.sub-menu').innerHTML += a_as_childes2;
+    
     has_childes[i].addEventListener('click', function() {
       let has_childes_child_nodes = this.querySelectorAll('.sub-menu')
       for(e = 0; e < has_childes_child_nodes.length; e++) {
-        has_childes_child_nodes[e].classList.toggle('show');
-        has_childes_child_nodes[e].classList.toggle('notshow');
-        console.log('hi');
+
+        if(has_childes_child_nodes[e].style.display == 'none') {
+          has_childes_child_nodes[e].style.display = 'block';
+        } else {
+          has_childes_child_nodes[e].style.display = 'none';
+        }
       }
     })
+  }
+
+  all_sub_menus = document.querySelectorAll('.sub-menu')
+
+  for(z = 0; z < all_sub_menus.length; z++) {
+    all_sub_menus[z].style.left = '0%'
   }
 }
 
