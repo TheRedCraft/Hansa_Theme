37a0d8ac31d5c7d1a142fc22e9afc533eb78e54b
TheRedCraft <111310570+TheRedCraft@users.noreply.github.com>
2022-11-01 13:37:56 +0100

    Update Path

--- single.php
+++ single.php
@@ -45,7 +45,8 @@ if ($banana == 'true') {
   echo "</div>";
   echo "</div>";
   echo "<script src='/wordpress/wp-content/themes/Hansa_Theme/calender.js'></script>";
-  $file = fopen("http://wp.hansa-gymnasium.de/httpdocs/wp-content/themes/Hansa_Theme-master/calenderimgparsed.txt","r");
+  $file = fopen("http://wp.hansa-gymnasium.de/var/www/vhosts/wp.hansa-gymnasium.de/httpdocs/wp-content/themes/Hansa_Theme-master/calenderimgparsed.txt","r");
+
 
   while(! feof($file))
     {
